0bc8c0b8dfa9e524ef68700bb4d419bf38983fac
Caleb Kleveter <caleb.kleveter@gmail.com>
2020-01-21 15:15:13 -0600

    Added SwiftNIO and SwiftCoroutines dependencies

--- Package.swift
+++ Package.swift
@@ -1,28 +1,18 @@
 // swift-tools-version:5.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "NIOCoroutines",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "NIOCoroutines",
-            targets: ["NIOCoroutines"]),
+        .library(name: "NIOCoroutines", targets: ["NIOCoroutines"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.0.0"),
+        .package(url: "https://github.com/belozierov/SwiftCoroutine.git", from: "1.0.5")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "NIOCoroutines",
-            dependencies: []),
-        .testTarget(
-            name: "NIOCoroutinesTests",
-            dependencies: ["NIOCoroutines"]),
+        .target(name: "NIOCoroutines", dependencies: ["NIO", "SwiftCoroutine"]),
+        .testTarget(name: "NIOCoroutinesTests", dependencies: ["NIOCoroutines", "NIO"]),
     ]
 )
